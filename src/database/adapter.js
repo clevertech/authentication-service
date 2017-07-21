5231146e7e51979d3d4f7a2e0a53526fbfc2bc0c
Ian McDevitt <ian@clevertech.biz>
2017-07-21 10:01:29 -0400

    Move relational.js back to knex.js, and put them in the same directory

--- src/database/adapter.js
+++ src/database/adapter.js
@@ -1,13 +1,13 @@
 module.exports = env => {
-  // If we have a relational database, just grab the relational adapter
+  // If we have a relational database, just grab the knex adapter
   if (['mysql', 'pg'].indexOf(env('DATABASE_ENGINE')) !== -1) {
-    return require('./adapters/relational')(env)
+    return require('./knex')(env)
   // Otherwise, grab the specific database adapter
   } else if (env('DATABASE_ENGINE') === 'mongodb') {
-    return require('./adapters/mongodb')(env)
+    return require('./mongodb')(env)
   // If we don't have a DATABASE_ENGINE, default to postgres (as we had before)
   } else {
     env('DATABASE_ENGINE', 'pg')
-    return require('./adapters/relational')(env)
+    return require('./knex')(env)
   }
 }
--- src/database/knex.js
+++ src/database/knex.js
@@ -1,5 +1,5 @@
 const knex = require('knex')
-const constants = require('../../constants')
+const constants = require('../constants')
 const _ = require('lodash')
 
 module.exports = env => {

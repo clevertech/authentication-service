9769aaec4d3e8fcacf315c6e92a7fca19837d01f
Ian McDevitt <ian@clevertech.biz>
2017-07-31 16:35:48 -0400

    Fix tests to be properly async

--- test/database-mysql.js
+++ test/database-mysql.js
@@ -4,7 +4,69 @@ const env = require('../src/utils/env')({
   DATABASE_URL: 'mysql://root:root@localhost/auth'
 })
 const adapter = require('../src/database/adapter')(env)
+const userId = parseInt(Math.random() * Number.MAX_SAFE_INTEGER)
 
-test('Init', t => {
-  adapter.init().then(res => t.truthy(res)).catch(err => t.falsy(res))
+test.serial('init()', t => {
+  t.plan(1)
+  return adapter.init()
+    .then(res => {
+      // We're not concerned with the output; just that it didn't error
+      t.pass()
+    }).catch(err => {
+      t.falsy(res)
+    })
+})
+
+test.serial('insertUser()', t => {
+  t.plan(1)
+  return adapter.insertUser({ id: userId, email: `test+${userId}@clevertech.biz`, emailConfirmationToken: `token${userId}`})
+    .then(res => {
+      // We're not concerned with the output; just that it didn't error
+      t.pass()
+    }).catch(err => {
+      t.falsy(err)
+    })
+})
+
+test.serial('insertProvider()', t => {
+  t.plan(1)
+  return adapter.insertProvider({ userId, login: `login${userId}`, data: 'placeholder string' })
+    .then(res => {
+      // We're not concerned with the output; just that it didn't error
+      t.pass()
+    }).catch(err => {
+      t.falsy(err)
+    })
+})
+
+test('updateUser()', t => {
+  t.plan(3)
+  return adapter.updateUser({ id: userId, emailConfirmed: 1, termsAndConditions: 1 })
+    .then(updateCount => {
+      console.log('tested the count')
+      return adapter.findUserById(userId).then(res => {
+        console.log('doing a thing');
+        t.is(updateCount, 1)
+        t.is(res.emailConfirmed, 1)
+        t.is(res.termsAndConditions, "1")
+      })
+    }).catch(err => {
+      t.falsy(err)
+    })
+})
+
+test('findUserByEmail()', t => {
+
+})
+
+test('findUserByEmailConfirmationToken()', t => {
+
+})
+
+test('findUserById()', t => {
+
+})
+
+test('findUserByProviderLogin()', t => {
+
 })

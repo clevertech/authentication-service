8ec9b4f5a847215983cfdcb236edbf8586a08602
Ian McDevitt <ian@clevertech.biz>
2017-07-31 16:42:52 -0400

    Finish MySQL adapter tests

--- test/database-mysql.js
+++ test/database-mysql.js
@@ -43,9 +43,7 @@ test('updateUser()', t => {
   t.plan(3)
   return adapter.updateUser({ id: userId, emailConfirmed: 1, termsAndConditions: 1 })
     .then(updateCount => {
-      console.log('tested the count')
       return adapter.findUserById(userId).then(res => {
-        console.log('doing a thing');
         t.is(updateCount, 1)
         t.is(res.emailConfirmed, 1)
         t.is(res.termsAndConditions, "1")
@@ -56,17 +54,50 @@ test('updateUser()', t => {
 })
 
 test('findUserByEmail()', t => {
-
+  t.plan(3)
+  return adapter.findUserByEmail(`test+${userId}@clevertech.biz`)
+    .then(res => {
+      t.is(res.id, `${userId}`)
+      t.is(res.email, `test+${userId}@clevertech.biz`)
+      t.is(res.emailConfirmationToken, `token${userId}`)
+    }).catch(err => {
+      t.falsy(err)
+    })
 })
 
 test('findUserByEmailConfirmationToken()', t => {
-
+  t.plan(3)
+  return adapter.findUserByEmailConfirmationToken(`token${userId}`)
+    .then(res => {
+      t.is(res.id, `${userId}`)
+      t.is(res.email, `test+${userId}@clevertech.biz`)
+      t.is(res.emailConfirmationToken, `token${userId}`)
+    }).catch(err => {
+      t.falsy(err)
+    })
 })
 
 test('findUserById()', t => {
-
+  t.plan(3)
+  return adapter.findUserById(userId)
+    .then(res => {
+      t.is(res.id, `${userId}`)
+      t.is(res.email, `test+${userId}@clevertech.biz`)
+      t.is(res.emailConfirmationToken, `token${userId}`)
+    }).catch(err => {
+      t.falsy(err)
+    })
 })
 
 test('findUserByProviderLogin()', t => {
-
+  t.plan(4)
+  return adapter.findUserByProviderLogin(`login${userId}`)
+    .then(res => {
+      t.is(res.id, `${userId}`)
+      t.is(res.email, `test+${userId}@clevertech.biz`)
+      t.is(res.emailConfirmationToken, `token${userId}`)
+      t.is(res.login, `login${userId}`)
+    }).catch(err => {
+      t.falsy(err)
+    })
 })

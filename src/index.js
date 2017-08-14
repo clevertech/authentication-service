93d15d5bc4662ae57845490d945dea4b76b3cbaf
Ian McDevitt <ian@clevertech.biz>
2017-08-14 19:27:43 -0400

    Uncomment captcha

--- src/index.js
+++ src/index.js
@@ -120,7 +120,7 @@ exports.createRouter = (config = {}) => {
 
   const router = express.Router()
   router.use(bodyParser.urlencoded({ extended: false }))
-  //router.use(recaptcha.middleware())
+  router.use(recaptcha.middleware())
   router.use(i18n.init)
 
   const availableProviders = Object.keys(providers).reduce((obj, provider) => {

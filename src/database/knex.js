f54a00eb683dd9beea6589cfc1ab83ee61ab6aed
Ian McDevitt <ian@clevertech.biz>
2017-08-14 19:26:41 -0400

    Undo unneeded change

--- src/database/knex.js
+++ src/database/knex.js
@@ -60,7 +60,7 @@ module.exports = env => {
                 }, Promise.resolve([]))
                 .then(missing => {
                   if (missing) {
-                    return db.schema.alterTable(
+                    return db.schema.createTableIfNotExists(
                       'auth_users',
                       _.once(table => {
                         missing.forEach(fieldName => table.string(fieldName))

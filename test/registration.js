aa5e7d316697bebd17fc743e22ccbffc045bcbcd
Ian McDevitt <ian@clevertech.biz>
2017-08-17 12:02:23 -0400

    Use an email address that isn't mine :simple_smile:

--- test/registration.js
+++ test/registration.js
@@ -34,7 +34,7 @@ test.serial('POST /auth/register', t => {
     .send(`firstName=Ian`)
     .send(`lastName=McDevitt`)
     .send(`username=ian`)
-    .send(`email=ian%2B${r}@clevertech.biz`)
+    .send(`email=test%2B${r}@clevertech.biz`)
     .send(`password=thisistechnicallyapassword`)
     .then((response) => {
       t.truthy(response.text.indexOf('<p>Before signing in, please confirm your email address.') >= 0)
@@ -50,21 +50,21 @@ let token
 // Mark that account as having a confirmed email address
 // Then sign into that account
 test.serial('POST /auth/signin', t => {
-  return db.findUserByEmail(`ian+${r}@clevertech.biz`)
+  return db.findUserByEmail(`test+${r}@clevertech.biz`)
   .then(user => {
     user.emailConfirmed = true
     return db.updateUser(user)
       .then(success => {
         t.truthy(success)
         return superagent.post(`${baseUrl}/auth/signin`)
-          .send(`email=ian%2B${r}@clevertech.biz`)
+          .send(`email=test%2B${r}@clevertech.biz`)
           .send('password=thisistechnicallyapassword')
           .then((response) => {
             // Store the JWT for later use
             token = response.text
             // Confirm that the JWT does indeed contain the data we want
             const decoded = jwt.decode(token)
-            t.is(decoded.user.email, `ian+${r}@clevertech.biz`)
+            t.is(decoded.user.email, `test+${r}@clevertech.biz`)
           })
           .catch((error) => {
             t.falsy(error)
